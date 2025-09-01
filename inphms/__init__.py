3b9af4dc67dde22218a3ae6f33a311dc9a5311eb
Yancovenant <tirtamoto@gmail.com>
2025-09-01 17:18:48 +0700

    u

--- inphms/__init__.py
+++ inphms/__init__.py
@@ -3,6 +3,10 @@
 
 """ INPHMS core library. """
 
+# ----------------------------------------------------------
+# Inphms must be a namespace package for inphms.addons to become one too
+# https://packaging.python.org/guides/packaging-namespace-packages/
+# ----------------------------------------------------------
 import pkgutil
 import os.path
 __path__ = [
@@ -21,6 +25,7 @@ assert sys.version_info > MIN_PY_VERSION, f"Outdated python version detected, In
 # The hard-coded super-user id (a.k.a. administrator, or root user).
 SUPERUSER_ID = 1
 
+
 # ----------------------------------------------------------
 # Import tools to patch code and libraries
 # required to do as early as possible for evented and timezone
@@ -28,11 +33,15 @@ SUPERUSER_ID = 1
 from . import _monkeypatches
 _monkeypatches.patch_all()
 
+
 # ----------------------------------------------------------
 # Imports
 # ----------------------------------------------------------
 # from . import upgrade  # this namespace must be imported first
 # from . import addons
+# from . import conf
+# from . import loglevels
+# from . import osv
 from . import release
 from . import netsvc
 from . import modules
@@ -44,6 +53,9 @@ from . import tools
 ## MODEL CLASSES
 from . import api
 from . import models
+# from . import fields
+# from inphms.tools.translate import _, _lt
+# from inphms.fields import Command
 
 ## OTHER IMPORT REQUIRED
 from . import cli
